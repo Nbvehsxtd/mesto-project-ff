0e233143a46a7f0410d40a44b995cb23e28c202f
Timur <Nbvehsxt@yandex.ru>
2023-10-20 12:01:46 +0300

    комментарий к коммиту

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const placesList = document.querySelector('.places__list');
 const template = document.querySelector('#card-template').content; 
+
 initialCards.forEach(function (item) {
 const name = item.name;
 const link = item.link;
@@ -7,15 +8,15 @@ addCard(name, link);
 });
 
 function addCard(name, link) {
-    const card = template.cloneNode(true);
-    card.querySelector('.card__title').textContent = name;
-    card.querySelector('.card__image').src = link;
-    card.querySelector('.card__image').alt = name + ', фото';
-    placesList.append(card);
+  const card = template.cloneNode(true);
+  card.querySelector('.card__title').textContent = name;
+  card.querySelector('.card__image').src = link;
+  card.querySelector('.card__image').alt = name + ', фото';
+  placesList.append(card);
 };
   
 document.querySelector('body').onclick = function(e) {
-    if(e.target.className != 'card__delete-button') return;
-    const item = e.target.closest('.places__item');
-    item.remove();
+  if(e.target.className != 'card__delete-button') return;
+  const item = e.target.closest('.places__item');
+  item.remove();
 };
